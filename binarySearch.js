a89d3c92dd05fd92f432acc9f035ef8265d7e915
gautamKumar <gautamkannoujiya58@gmail.com>
2024-10-01 23:09:39 +0530

    1st problem simple Binary Search (BS) added

---
+++ binarySearch.js
@@ -0,0 +1,23 @@
+console.log("Hello world");
+function binarySearch(arr, targetElmt) {
+  let len = arr.length;
+  let start = 0;
+  let end = len - 1;
+
+  while (start <= end) {
+    let mid = start + Math.floor((end - start) / 2);
+    if (targetElmt === arr[mid]) {
+      return mid;
+    }
+    if (targetElmt < arr[mid]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return -1;
+}
+console.log(binarySearch([1, 3, 5, 7, 8, 90], 90)); // 5
+console.log(binarySearch([1, 3, 90, 800, 998, 900], 0)); // -1
+console.log(binarySearch([3, 11, 16, 34, 89, 92, 120], 120)); // 6
+console.log(binarySearch([9, 11, 45, 49, 60], 11)); // 1
